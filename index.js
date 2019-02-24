ffb34cd756e07b8f58c275d30c0f2d518c069afa
Anton <sochianton@gmail.com>
2019-02-24 22:59:29 +0300

    First steps in pug

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-/**
- *
- * Created by anton on 24.02.19.
- *
- */
+let express = require('express');
+let _ = require('lodash');
+
+
+let app = express();
+app.listen(3000);
+app.use('/assets', express.static(__dirname + '/public'));
+app.set('view engine', 'pug');
+
+let params = {
+    defaultTitle: 'App title'
+};
+
+// GET method route
+app.get('/', function (req, res) {
+    res.render('index', {
+        _,
+        params
+    });
+});
+
+// POST method route
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage');
+});
+
